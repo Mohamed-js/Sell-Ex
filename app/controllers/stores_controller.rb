f46685181fe0b2de9f83445a0ddf1fdcc4d8fa90
Mohamed Atef <atefhamosa@gmail.com>
2023-02-01 04:12:43 -0800

    Update stores_controller.rb

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -100,6 +100,7 @@ class StoresController < ApplicationController
   end
 
   def refresher
+    Net::HTTP.get(URI('https://appyrefresher.onrender.com/refresher'))
     render json: 'I\'m good!'
   end
   private
