67fb1c71bc10d9ce643c3b657748b9103fda019c
Mohamed Atef <atefhamosa@gmail.com>
2023-04-05 03:39:10 +0200

    Add stores endpoint

--- app/controllers/api/v1/stores_controller.rb
+++ app/controllers/api/v1/stores_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::StoresController < Api::V1::ApiController  
     def index
         # render json: OrderItem.joins(:product, :category).group('UPPER(categories.name)').sum('order_items.quantity * price')
-        render json: OrderItem.joins(:product, :category).group('UPPER(categories.name)').sum('order_items.quantity * price')
+        render json: Store.all
     end
 
     def show
