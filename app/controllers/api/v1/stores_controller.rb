5db9bf6ee6083e004c1b793a591a50cee754704d
Mohamed Atef <atefhamosa@gmail.com>
2023-04-05 04:49:27 +0200

    Update stores_controller.rb

--- app/controllers/api/v1/stores_controller.rb
+++ app/controllers/api/v1/stores_controller.rb
@@ -1,4 +1,5 @@
-class Api::V1::StoresController < Api::V1::ApiController  
+class Api::V1::StoresController < Api::V1::ApiController
+    skip_before_action :set_store, only: :index  
     def index
         # render json: OrderItem.joins(:product, :category).group('UPPER(categories.name)').sum('order_items.quantity * price')
         render json: Store.all
