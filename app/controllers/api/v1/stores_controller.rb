f72b7e77dce59f7c182134e574cc9ff2dc97582a
Mohamed Atef <atefhamosa@gmail.com>
2023-04-05 03:38:41 +0200

    Update stores_controller.rb

--- app/controllers/api/v1/stores_controller.rb
+++ app/controllers/api/v1/stores_controller.rb
@@ -1,14 +1,10 @@
 class Api::V1::StoresController < Api::V1::ApiController  
-    def show
-        render json: @store
+    def index
+        # render json: OrderItem.joins(:product, :category).group('UPPER(categories.name)').sum('order_items.quantity * price')
+        render json: OrderItem.joins(:product, :category).group('UPPER(categories.name)').sum('order_items.quantity * price')
     end
 
-    # TESTER
-    def index
-        # res = {}
-        # Category.all.each {|category| res["#{category.name}"] = category.order_items.sum("order_items.price * order_items.quantity")}
-        # render json: res
-
-        render json: OrderItem.joins(:product, :category).group('UPPER(categories.name)').sum('order_items.quantity * price')
+    def show
+        render json: @store
     end
 end
