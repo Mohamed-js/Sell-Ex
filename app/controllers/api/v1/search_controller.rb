e3bc2097124baa85a747d90fdf85adf5e810ea96
Mohamed Atef <atefhamosa@gmail.com>
2023-09-12 23:40:09 +0300

    Add category to products search

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -2,6 +2,8 @@ class Api::V1::SearchController < Api::V1::ApiController
     def index
         q = ActiveRecord::Base::sanitize_sql(params[:q])
 
-        render json: @store.products.active.where("LOWER(CONCAT(name, description)) LIKE ?", "%#{q}%".downcase), except: [:category_id, :quantity, :image_id], status: :ok
+        render json: @store.products.active.where("LOWER(CONCAT(name, description)) LIKE ?", "%#{q}%".downcase),
+        except: [:category_id, :quantity, :image_id],
+        include:{ :category => {:only => [:name]} }, status: :ok
     end
 end
